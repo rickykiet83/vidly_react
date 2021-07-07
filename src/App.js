155c9848bea696abbb0fb4180907aef03b51e55a
Kiet Pham <kietpham.dev@gmail.com>
2021-07-07 22:28:23 +1000

    Building the movies component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Movies from './components/movies';
+import React from 'react';
+
 function App() {
   return (
     <main className='container'>
-      <h1>Hello World</h1>
+      <Movies />
     </main>
   );
 }
