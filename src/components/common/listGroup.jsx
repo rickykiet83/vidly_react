bd8402d14571f04540250886588b5a53a542a4d0
Kiet Pham <kietpham.dev@gmail.com>
2021-08-05 20:12:51 +1000

    Filtering - Displaying items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const ListGroup = () => {
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
   return (
-    <div>
-      <ul className='list-group'>
-        <li className='list-group-item'>An item</li>
-        <li className='list-group-item'>A second item</li>
-        <li className='list-group-item'>A third item</li>
-        <li className='list-group-item'>A fourth item</li>
-        <li className='list-group-item'>And a fifth one</li>
-      </ul>
-    </div>
+    <ul className='list-group'>
+      {items.map((item) => (
+        <li key={item[valueProperty]} className='list-group-item'>
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
   );
 };
 
