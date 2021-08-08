02ae9395cfa94851ee7af95fe99dc21b62a8f3aa
Kiet Pham <kietpham.dev@gmail.com>
2021-08-08 23:34:33 +1000

    Sorting Unique key

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -30,7 +30,7 @@ class MoviesTable extends Component {
   ];
 
   render() {
-    const { movies, onLike, onDelete, sortColumn, onSort } = this.props;
+    const { movies, sortColumn, onSort } = this.props;
     return (
       <table className='table'>
         <TableHeader
@@ -39,27 +39,6 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
         <TableBody data={movies} columns={this.columns} />
-        {/* <tbody>
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onClick={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className='btn btn-danger btn-sm'
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
