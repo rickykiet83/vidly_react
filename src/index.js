23efb3cb327ece4ae10f6790fc25897d6b7febcc
Kiet Pham <kietpham.dev@gmail.com>
2021-08-09 22:37:31 +1000

    Adding Routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
