c09261bea1093ff5deb6884c854e1600ceba3510
Kiet Pham <kietpham.dev@gmail.com>
2021-08-24 11:39:01 +1000

    Handling form submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,32 +1,39 @@
 import { Component } from 'react';
 
 class LoginForm extends Component {
-  state = {};
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submitted');
+  };
+
   render() {
     return (
       <div>
         <h2>Login</h2>
-        <div className='mb-3 col'>
-          <label htmlFor='email' className='col-sm-2 col-form-label'>
-            Email
-          </label>
-          <div className='col-sm-4'>
-            <input type='password' className='form-control' id='email' />
+        <form onSubmit={this.handleSubmit}>
+          <div className='mb-3 col'>
+            <label htmlFor='email' className='col-sm-2 col-form-label'>
+              Email
+            </label>
+            <div className='col-sm-4'>
+              <input type='password' className='form-control' id='email' />
+            </div>
           </div>
-        </div>
-        <div className='mb-3 col'>
-          <label htmlFor='password' className='col-sm-2 col-form-label'>
-            Password
-          </label>
-          <div className='col-sm-4'>
-            <input type='password' className='form-control' id='password' />
+          <div className='mb-3 col'>
+            <label htmlFor='password' className='col-sm-2 col-form-label'>
+              Password
+            </label>
+            <div className='col-sm-4'>
+              <input type='password' className='form-control' id='password' />
+            </div>
           </div>
-        </div>
-        <div className='col col-sm-2'>
-          <button type='submit' className='btn btn-primary mb-3'>
-            Login
-          </button>
-        </div>
+          <div className='col col-sm-2'>
+            <button type='submit' className='btn btn-primary mb-3'>
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
