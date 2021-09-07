1fe8d886272cee9061d52c2a814f307224412c0c
Kiet Pham <kietpham.dev@gmail.com>
2021-09-07 18:01:15 +1000

    Fixing circle dependencies

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,6 @@
-import auth from './authService';
 import axios from 'axios';
 import logger from './logService';
 import { toast } from 'react-toastify';
-
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -19,9 +16,14 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
+export function setJwt(jwt) {
+  axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
+  setJwt,
 };
