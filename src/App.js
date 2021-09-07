3b6f665f2a822fda941bc807d38f41aa5e295812
Kiet Pham <kietpham.dev@gmail.com>
2021-09-07 20:45:18 +1000

    Showing or Hiding Elements based on the User

--- src/App.js
+++ src/App.js
@@ -35,7 +35,10 @@ class App extends Component {
             <Route path='/logout' component={Logout}></Route>
             <Route path='/register' component={RegisterForm}></Route>
             <Route path='/movies/:id' component={MovieForm}></Route>
-            <Route path='/movies' component={Movies}></Route>
+            <Route
+              path='/movies'
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            ></Route>
             <Route path='/customers' component={Customers}></Route>
             <Route path='/rentals' component={Rentals}></Route>
             <Route path='/not-found' component={NotFound}></Route>
